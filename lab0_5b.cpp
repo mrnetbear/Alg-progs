056bcee3c8a8cbee3e0bcd2dbe6ae2c0056e1678
Alexey Konotop <90930648+mrnetbear@users.noreply.github.com>
2025-03-03 21:27:48 +0300

    completed lab 0_5b

--- lab0_5b.cpp
+++ lab0_5b.cpp
@@ -36,7 +36,9 @@ void append(Node** head_ref, const char new_data){
 Node* to_list(const std::string& str){
     Node* head = nullptr;
     for (auto it : str){
-        append(&head, it);
+        if (isdigit(it) || it == '\t' || it == ' ')
+            append(&head, it);
+        else throw std::invalid_argument("Invalid digit: " + std::string(1, it)); //exception
     }
     return head;
 }
@@ -50,12 +52,57 @@ void print_list(Node* head){
     std::cout << std::endl;
 }
 
+void list_transform(Node* head, Node** evenHead, Node** oddHead){
+    size_t i = 1;
+    bool fIsDigit = false;
+    Node* current = head;
+    std::cout << "busy... preparing loop..." << std::endl;
+    while(current != nullptr){
+        if(isdigit(current->data)){
+            if (i % 2){
+                append(oddHead, current->data);
+            }
+            else {
+                append(evenHead, current->data);
+            }
+            ++i;
+            fIsDigit = true;
+            std::cout << "busy... digit found!" << std::endl;
+
+        }
+        else if (fIsDigit){
+            append(evenHead, ' ');
+            append(oddHead, ' ');
+            i = 1;
+            fIsDigit = false;
+            std::cout << "busy... space found!" << std::endl;
+
+        }
+        current = current->next; 
+    }
+    std::cout << "busy... loop is done!" << std::endl;
+}
+
 int main(){
     std::string line;
 
-    while (std::getline(std::cin, line)) {    
-        Node* crr_line = to_list(line);
-        print_list(crr_line);
+    while (std::getline(std::cin, line)) {   
+        Node* crr_line = nullptr;
+        Node* evenHead = nullptr;
+        Node* oddHead = nullptr;
+        try{ 
+            crr_line = to_list(line);
+            print_list(crr_line);
+            list_transform(crr_line, &evenHead, &oddHead);
+            std::cout << "Even: ";
+            print_list(evenHead);
+            std::cout << "Odd: ";
+            print_list(oddHead);
+            std::cout << std::endl;
+        } catch (const std::invalid_argument& e) {
+            std::cerr << e.what() << '\n';
+        }
+        
     }
     return 0;
 }
