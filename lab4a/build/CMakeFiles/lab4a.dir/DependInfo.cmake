8dd7fbcad791a9970eda9e3b985545045c91d095
Alexey Konotop <90930648+mrnetbear@users.noreply.github.com>
2025-04-28 23:05:20 +0300

    added cmake

---
+++ lab4a/build/CMakeFiles/lab4a.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mcsquare/Documents/Alg-progs/lab4a/src/bintree.cpp" "CMakeFiles/lab4a.dir/src/bintree.cpp.o" "gcc" "CMakeFiles/lab4a.dir/src/bintree.cpp.o.d"
+  "/Users/mcsquare/Documents/Alg-progs/lab4a/src/lab4a.cpp" "CMakeFiles/lab4a.dir/src/lab4a.cpp.o" "gcc" "CMakeFiles/lab4a.dir/src/lab4a.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab4a/build/CMakeFiles/lab4a.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4a.dir/src/bintree.cpp.o"
+  "CMakeFiles/lab4a.dir/src/bintree.cpp.o.d"
+  "CMakeFiles/lab4a.dir/src/lab4a.cpp.o"
+  "CMakeFiles/lab4a.dir/src/lab4a.cpp.o.d"
+  "lab4a"
+  "lab4a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
