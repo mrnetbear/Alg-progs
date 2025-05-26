00e692cfbbd952541a7fd155a02a980c5efddf54
mrnetbear <akonotop03@mail.ru>
2025-05-26 15:09:47 +0300

    tmp savestate

--- labmsu/src/connectedSets.hpp
+++ labmsu/src/connectedSets.hpp
@@ -25,6 +25,34 @@ struct PointNode{
 class PointList {
     PointNode* head;
     PointNode* tail;
+    size_t size_;
+
+public:
+    PointList() : head(nullptr), tail(nullptr), size_(0) {}
+    
+    ~PointList() {
+        clear();
+    }
+
+    void push_back(const Point& p);
+    bool contains(const Point& p) const;
+    bool isNear(const Point& p) const;
+    void merge(const PointList& other);
+    void clear();
+    size_t size() const;
+};
+
+class Iterator {
+    PointNode* current;
+
+public: 
+    Iterator(PointNode* node) : current(node) {}
+    Point& operator*() { return current->data; }
+    Iterator& operator++() { current = current->next; return *this; }
+    bool operator!=(const Iterator& other) { return current != other.current; }
+
+    Iterator begin() { return Iterator(current); }
+    Iterator end() { return Iterator(nullptr); }
 };
 
 #endif //CONNECTED_SETS_H
